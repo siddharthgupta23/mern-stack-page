cae05ceb06a3f229c31e2cf73e12fc127f64ac5d
Siddharth Gupta <siddharthgupta1603@gmail.com>
2025-01-29 16:18:36 +0530

    initial commit

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -1,10 +1,7 @@
 
-import Hero from "../components/Hero"
-import CompanyLogos from "../components/CompanyLogos"
-import Benefits from "../components/Benefits"
+
 import GlobalPresence from "../components/GlobalPresence"
-import Stats from "../components/Stats"
-import CaseStudies from "../components/CaseStudies"
+
 import BlogPosts from "../components/BlogPosts"
 import CallToAction from "../components/CallToAction"
 import Footer from "../components/Footer"
